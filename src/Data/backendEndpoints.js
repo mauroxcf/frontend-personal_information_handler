11878b5f62a16919d5eed4626c773ffb2fb71151
mauroxcf <maurox03@gmail.com>
2022-09-09 23:30:59 -0500

    fix endpoints

--- src/Data/backendEndpoints.js
+++ src/Data/backendEndpoints.js
@@ -5,9 +5,9 @@ const createPersonUrl = 'http://localhost:5000/new';
 const updatePersonUrl = 'http://localhost:5000/update/'; */
 
 /* production endpoints */
-const getAllPersonUrl = 'http://mcdeveloper03.pythonanywhere.com/all';
-const deletePersonUrl = 'http://mcdeveloper03.pythonanywhere.com/delete/';
-const createPersonUrl = 'http://mcdeveloper03.pythonanywhere.com/new';
-const updatePersonUrl = 'http://mcdeveloper03.pythonanywhere.com/update/';
+const getAllPersonUrl = 'https://mcdeveloper03.pythonanywhere.com/all';
+const deletePersonUrl = 'https://mcdeveloper03.pythonanywhere.com/delete/';
+const createPersonUrl = 'https://mcdeveloper03.pythonanywhere.com/new';
+const updatePersonUrl = 'https://mcdeveloper03.pythonanywhere.com/update/';
 
 export { getAllPersonUrl, deletePersonUrl, createPersonUrl, updatePersonUrl };
